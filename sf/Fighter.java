1e5b6747011014188356296d73500827658338af
kxue7438 <kxue7438@CHHB2ZHKXV1.inst.hcpss.org>
2019-04-12 12:52:38 -0400

    awfeaswfeffefefewfefefefefefefefegfefefefef

--- sf/Fighter.java
+++ sf/Fighter.java
@@ -4,52 +4,49 @@ import java.awt.Graphics;
 
 public class Fighter {
 	private SpriteSheet s;
-	private AnimatedSprite ryuIdle;
-	private AnimatedSprite ryuWalk;
-	private AnimatedSprite ryu;
+	private AnimatedSprite idle;
 	private int x;
 	private int y;
 	private int velx;
 	private int vely;
 	private int grav = 5;
-		
+	
+	
 	public Fighter(String filename) {
 		
 		s = new SpriteSheet(filename);
-		ryuIdle = new AnimatedSprite(s, 4, new int[][] {{0, 10, 50, 90},
-														{50, 10, 50, 90},
-														{100, 10, 50, 90},
-														{150, 10, 50, 90}});
-		ryuWalk = new AnimatedSprite(s, 3, new int[][] {{200, 10, 40, 90},
-														{250, 10, 40, 90},
-														{300, 10, 40, 90},
-														{350, 10, 40, 90},
-														{400, 10, 40, 90}});
+		
+		idle = new AnimatedSprite(s,4, new int[][] {{0, 10, 50, 90},
+													{50, 10, 50, 90},
+													{100, 10, 50, 90},
+													{150, 10, 50, 90}});
 		
 		if(filename.equals("ryu good transparent.png")) {
 			
 		}
 		x = 100;
 		y = 100;
-		velx = 5;
-		vely = 0;
+		velx = 2;
+		vely = 10;
 	}
 	public SpriteSheet getSheet() {
 		return s;
 	}
 	public void draw(Graphics g, int frames) {
-		g.drawImage(ryuWalk.update(frames, 60), 100, 100, null);
+		g.drawImage(idle.update(frames,1), x, y, null);
+		System.out.println(x);
+		System.out.println(y);
 	}
-	public double getX() {
+	public int getX() {
 		return x;
 	}
-	public double getY() {
+	public int getY() {
 		return y;
 	}
-	public double getVelx() {
+	public int getVelx() {
 		return velx;
 	}
-	public double getVely() {
+	public int getVely() {
 		return vely;
 	}
 	public void setX(int a) {
