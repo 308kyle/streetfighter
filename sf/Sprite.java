5b3dea3e82d02f7b8f1e30810e4bcff34b46b997
kxue7438 <kxue7438@gmail.com>
2019-05-08 12:27:18 -0400

    s

--- sf/Sprite.java
+++ sf/Sprite.java
@@ -1,5 +1,39 @@
 package sf;
 
-public class Sprite {
+import java.awt.Rectangle;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
 
+import javax.imageio.ImageIO;
+
+public class Sprite {
+	private ArrayList<Rectangle> box;
+	private int dx;
+	private int dy;
+	private int x;
+	private int y;
+	private int x1;
+	private int y1;
+	private int w;
+	private int h;
+	private BufferedImage img;
+	public Sprite( int x, int y, int x1, int y1, int dx, int dy, SpriteSheet s) {
+		w = x1 - x + 1;
+		h = y1 - y + 1;
+		img = s.getSprite(x, y, w, h, 4);		
+	}
+	public BufferedImage getSprite() {
+		return img;
+	}
+	public int dx() {
+		return dx;
+	}
+	public int dy() {
+		return dy;
+	}
+	public void createBox() {
+		box.add(new Rectangle(w, h));
+	}
 }
