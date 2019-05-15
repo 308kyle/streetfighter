5fd5337843cefb1bd29bc013cb1cbe004a583cf4
OriginalUser <35042376+OriginalUser@users.noreply.github.com>
2019-05-15 02:54:57 -0400

    FFFFFF

--- sf/Fighter.java
+++ sf/Fighter.java
@@ -44,6 +44,10 @@ public class Fighter {
 		Sprite i2 = new Sprite(55, 19, 55+43-1, 19+80-1, 0, 0, s);
 		Sprite i3 = new Sprite(105, 18, 105+43-1, 18+81-1, 0, 0, s);
 		Sprite i4 = new Sprite(154, 17, 154+43-1, 17+82-1, 0, 0, s);
+		i1.createBox();
+		i2.createBox();
+		i3.createBox();
+		i4.createBox();
 		ryuIdle = new AnimatedSprite(0, 60, true, true, new Sprite[] {i1, i2, i3, i4});
 		
 		Sprite w1 = new Sprite(205, 24, 205+43-1, 24+75-1, 5, 0, s);
@@ -51,12 +55,20 @@ public class Fighter {
 		Sprite w3 = new Sprite(301, 18, 301+43-1, 18+81-1, 5, 0, s);
 		Sprite w4 = new Sprite(351, 19, 351+43-1, 19+80-1, 5, 0, s);
 		Sprite w5 = new Sprite(401, 19, 401+43-1, 19+80-1, 5, 0, s);
+		w1.createBox();
+		w2.createBox();
+		w3.createBox();
+		w4.createBox();
+		w5.createBox();
 		ryuWalk = new AnimatedSprite(0, 60, true, true, new Sprite[] {w1, w2, w3, w4, w5});
 		
 		Sprite p11 = new Sprite(3, 134, 3+43-1, 134+81-1, 0, 0, s);
 		Sprite p12 = new Sprite(52, 134, 52+57-1, 134+81-1, 0, 0, s);
 		p12.addBox(new Rectangle(30, 10, 31+1, 14+1));
 		Sprite p13 = new Sprite(117, 134, 117+43-1, 134+81-1, 0, 0, s);
+		p11.createBox();
+		p12.createBox();
+		p13.createBox();
 		ryuPunch = new AnimatedSprite(2, 15, false, false, new Sprite[] {p11, p12, p13});
 			
 		Sprite p21 = new Sprite(170, 134, 170+43-1, 134+81-1, 0, 0, s);
@@ -65,6 +77,11 @@ public class Fighter {
 		p23.addBox(new Rectangle(41, 12, 35+1, 14+1));
 		Sprite p24 = new Sprite(353, 130, 353+51-1, 130+85-1, 0, 0, s);
 		Sprite p25 = new Sprite(411, 134, 411+43-1, 134+81-1, 0, 0, s);
+		p25.createBox();
+		p21.createBox();
+		p22.createBox();
+		p23.createBox();
+		p24.createBox();
 		ryuPunch2 = new AnimatedSprite(5, 35, false, false, new Sprite[] {p21, p22, p23, p24, p25});
 		
 		Sprite j1 = new Sprite(452, 24, 452+43-1, 24+75-1, 0, -8, s);
@@ -74,12 +91,23 @@ public class Fighter {
         Sprite j5 = new Sprite(619, 17, 619+29-1, 17+78-1, 0, 8, s);
         Sprite j6 = new Sprite(656, 9, 656+33-1, 9+90-1, 0, 8, s);
         Sprite j7 = new Sprite(696, 24, 696+43-1, 24+75-1, 0, 8, s);
+        j1.createBox();
+		j2.createBox();
+		j3.createBox();
+		j4.createBox();
+		j5.createBox();
+		j6.createBox();
+		j7.createBox();
 		ryuJump = new AnimatedSprite(5, 49, false, false, new Sprite[] {j1, j2, j3, j4, j5, j6, j7});
 		
 		Sprite h1 = new Sprite(5, 760, 5+44-1, 760+77-1, 0, 0, s);
 		Sprite h2 = new Sprite(53, 761, 53+48-1, 761+76-1, 0, 0, s);
 		Sprite h3 = new Sprite(106, 771, 106+50-1, 771+66-1, 0, 0, s);
 		Sprite h4 = new Sprite(163, 754, 163+44-1, 754+83-1, 0, 0, s);
+		h1.createBox();
+		h2.createBox();
+		h3.createBox();
+		h4.createBox();
 		ryuHit = new AnimatedSprite(0, 28, false, true, new Sprite[] {h1, h2, h3, h4});
 		
 		Sprite had1 = new Sprite(4, 636, 5+4-1, 636+83-1, 0, 0, s);
@@ -87,12 +115,21 @@ public class Fighter {
 		Sprite had3 = new Sprite(131, 643, 131+67-1, 643+76-1, 0, 0, s);
 		Sprite had4 = new Sprite(202, 648, 202+92-1, 648+71-1, 0, 0, s);
 		Sprite had5 = new Sprite(299, 648, 299+71-1, 648+71-1, 0, 0, s);
+		had5.createBox();
+		had1.createBox();
+		had2.createBox();
+		had3.createBox();
+		had4.createBox();
 		ryuHad = new AnimatedSprite(5, 35, false, false, new Sprite[] {had1, had2, had3, had4, had5});
 		
 		Sprite fh1 = new Sprite(217, 757, 217+49-1, 757+80-1, 0, 0, s);
 		Sprite fh2 = new Sprite(271, 756, 271+53-1, 756+80-1, 0, 0, s);
 		Sprite fh3 = new Sprite(328, 753, 328+59-1, 753+84-1, 0, 0, s);
 		Sprite fh4 = new Sprite(391, 754, 391+44-1, 754+83-1, 0, 0, s);
+		fh1.createBox();
+		fh2.createBox();
+		fh3.createBox();
+		fh4.createBox();
 		ryuFHit = new AnimatedSprite(0, 28, false, false, new Sprite[] {fh1, fh2, fh3, fh4});
 		
 		Sprite v1 = new Sprite(6, 887, 6+44-1, 887+76-1, 0, 0, s);
@@ -105,6 +142,11 @@ public class Fighter {
 		Sprite ko3 = new Sprite(1295, 806, 1295+75-1, 806+31-1, 0, 0, s);
 		Sprite ko4 = new Sprite(1373, 789, 1373+73-1, 789+43-1, 0, 0, s);
 		Sprite ko5 = new Sprite(1450, 806, 1450+75-1, 806+31-1, 0, 0, s);
+		ko5.createBox();
+		ko1.createBox();
+		ko2.createBox();
+		ko3.createBox();
+		ko4.createBox();
 		ryuKO = new AnimatedSprite(3, 35, false, false, new Sprite[] {ko1, ko2, ko3, ko4, ko5});
 		
 		Sprite k1 = new Sprite(511, 767, 511+48-1, 767+65-1, 0, 0, s);
@@ -115,6 +157,14 @@ public class Fighter {
 		Sprite k6 = new Sprite(831, 734, 831+53-1, 734+103-1, 0, 0, s);
 		Sprite k7 = new Sprite(889, 765, 889+56-1, 765+72-1, 0, 0, s);
 		Sprite k8 = new Sprite(949, 742, 949+42-1, 742+95-1, 0, 0, s);
+		k1.createBox();
+		k2.createBox();
+		k3.createBox();
+		k4.createBox();
+		k5.createBox();
+		k6.createBox();
+		k7.createBox();
+		k8.createBox();
 		ryuKnock = new AnimatedSprite(0, 56, false, false, new Sprite[] {k1, k2, k3, k4, k5, k6, k7, k8});
 
 		
